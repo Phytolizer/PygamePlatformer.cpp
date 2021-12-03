e0ecf375ae31c0978355c3873d8960eed76d0726
Kyle Coffey <mariorules24@gmail.com>
2021-12-03 09:19:00 -0600

    factor out constants

--- src/PygamePlatformer.cpp
+++ src/PygamePlatformer.cpp
@@ -7,11 +7,14 @@
 
 #include <SDL.h>
 
+constexpr int SCREEN_WIDTH = 1200;
+constexpr int SCREEN_HEIGHT = 700;
+
 int main(int /*argc*/, char** /*argv*/)
 {
     GraphicsContext context;
-    const SmartWindow window{"Pygame Platformer",     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1200, 700,
-                             SDL_WINDOW_ALLOW_HIGHDPI};
+    const SmartWindow window{"Pygame Platformer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                             SCREEN_WIDTH,        SCREEN_HEIGHT,           SDL_WINDOW_ALLOW_HIGHDPI};
     const SmartRenderer renderer{window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC};
 
     bool quit = false;
